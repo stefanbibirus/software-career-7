365d9f69c480eee399f1e3f5455c10a79eb7562d
Stefan Bibirus <stefan11bibirus@gmail.com>
2023-10-28 12:19:07 +0300

    pascal

--- pascal/src/Main.java
+++ pascal/src/Main.java
@@ -1,22 +1,22 @@
-public class PascalTriangle {
+import java.util.Scanner;
 
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<>();
+public class Pascal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
 
-        List<Integer> firstRow = new ArrayList<>();
-        firstRow.add(1);
-        triangle.add(firstRow);
-        for (int i = 1; i < numRows; i++) {
-            List<Integer> previousRow = triangle.get(i - 1);
-            List<Integer> currentRow = new ArrayList<>();
-            currentRow.add(1);
-            for (int j = 0; j < previousRow.size() - 1; j++) {
-                currentRow.add(previousRow.get(j) + previousRow.get(j + 1));
+        int p=1;
+        for(int j=1; j<=n; ++j){
+            p = 1;
+            System.out.print("1 ");
+            for (int i = 1; i <= j; i++) {
+
+                p = p * (j - i +1) / i;
+                System.out.print(p + " ");
+
             }
-            currentRow.add(1);
-            triangle.add(currentRow);
+            System.out.println();
         }
-
-        return triangle;
     }
 }
